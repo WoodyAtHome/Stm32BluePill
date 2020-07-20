ff0d4af0a24e53f5e4063ddcca1177ed831aaa57
WoodyAtHome <mzmail@gmx.net>
2020-07-20 18:58:27 +0200

    geht nicht mehr

--- main.zig
+++ main.zig
@@ -1,7 +1,7 @@
 usingnamespace @import("stm32f103.zig");
 
 pub fn main() noreturn {
-    RCC.*.APB2ENR |= RCC_APB2Periph_GPIOC; // enable GPIOC clk
+    RCC.*.APB2ENR |= (RCC_APB2Periph_GPIOC | RCC_APB2Periph_USART1); // enable GPIOC clk
     GPIOC.*.CRH &= ~@as(u32, 0b1111 << 20); // PC13
     GPIOC.*.CRH |= @as(u32, 0b0010 << 20); // Out PP, 2MHz
 
--- stm32f103.zig
+++ stm32f103.zig
@@ -1,3 +1,8 @@
+// 12:30 neue Schicht da
+// 13:00 Übergabe?
+// ab 13:15 weg
+// 14:00 ? weg
+
 const std = @import("std");
 //  const model = @import("system_model.zig");
 usingnamespace @import("main.zig");
@@ -7,7 +12,6 @@ extern var __text_end: u32;
 extern var __data_start: u32;
 extern var __data_size: u32;
 extern var __bss_start: u32;
-extern var __bss_end: u32;
 extern var __bss_size: u32;
 extern var __stack_top: u32;
 
@@ -37,24 +41,100 @@ fn resetHandler() callconv(.C) noreturn {
     const data = @ptrCast([*]u8, &__data_start);
     const text_end = @ptrCast([*]u8, &__text_end);
     for (text_end[0..data_size]) |b, i| data[i] = b;
-
-    // clear the bss
-    asm volatile ("nop");
-    asm volatile ("nop");
-    asm volatile ("nop");
-
     const start = &__bss_start;
     const bss_size = __bss_size;
     const bss = @ptrCast([*]u8, &__bss_start);
     for (bss[0..bss_size]) |*b| b.* = 0;
-
-    asm volatile ("nop");
-    asm volatile ("nop");
-    asm volatile ("nop");
-
-    //counter = 7;
     systemInit();
     main();
+}
+
+// copied verbatim from STM32 SDK
+fn systemInit() void {
+    //* Reset the RCC clock configuration to the default reset state(for debug purpose) */
+    //* Set HSION bit */
+    RCC.*.CR |= @as(u32, 0x00000001);
+
+    //* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
+    RCC.*.CFGR &= @as(u32, 0xF8FF0000);
+
+    //* Reset HSEON, CSSON and PLLON bits */
+    RCC.*.CR &= @as(u32, 0xFEF6FFFF);
+
+    //* Reset HSEBYP bit */
+    RCC.*.CR &= @as(u32, 0xFFFBFFFF);
+
+    //* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
+    RCC.*.CFGR &= @as(u32, 0xFF80FFFF);
+
+    //* Disable all interrupts and clear pending bits  */
+    RCC.*.CIR = 0x009F0000;
+
+    //* Configure the System clock frequency, HCLK, PCLK2 and PCLK1 prescalers */
+    //* Configure the Flash Latency cycles and enable prefetch buffer */
+    setSysClock();
+
+    SCB.*.VTOR = FLASH_BASE | VECT_TAB_OFFSET; //* Vector Table Relocation in Internal FLASH. */
+}
+
+fn setSysClock() void {
+    var StartUpCounter: u32 = 0;
+    var HSEStatus: u32 = 0;
+
+    //* SYSCLK, HCLK, PCLK2 and PCLK1 configuration ---------------------------*/
+    //* Enable HSE */
+    RCC.*.CR |= @as(u32, RCC_CR_HSEON);
+
+    //* Wait till HSE is ready and if Time out is reached exit */
+    HSEStatus = RCC.*.CR & RCC_CR_HSERDY;
+    StartUpCounter += 1;
+    while ((HSEStatus == 0) and (StartUpCounter != HSE_STARTUP_TIMEOUT)) {
+        HSEStatus = RCC.*.CR & RCC_CR_HSERDY;
+        StartUpCounter += 1;
+    }
+
+    if ((RCC.*.CR & RCC_CR_HSERDY) != RESET) {
+        HSEStatus = 0x01;
+    } else {
+        HSEStatus = 0x00;
+    }
+
+    if (HSEStatus == 0x01) {
+        //* Enable Prefetch Buffer */
+        FLASH.*.ACR |= FLASH_ACR_PRFTBE;
+
+        //* Flash 2 wait state */
+        FLASH.*.ACR &= @as(u32, ~FLASH_ACR_LATENCY);
+        FLASH.*.ACR |= @as(u32, FLASH_ACR_LATENCY_2);
+
+        //* HCLK = SYSCLK */
+        RCC.*.CFGR |= @as(u32, RCC_CFGR_HPRE_DIV1);
+
+        //* PCLK2 = HCLK */
+        RCC.*.CFGR |= @as(u32, RCC_CFGR_PPRE2_DIV1);
+
+        //* PCLK1 = HCLK */
+        RCC.*.CFGR |= @as(u32, RCC_CFGR_PPRE1_DIV2);
+
+        //*  PLL configuration: PLLCLK = HSE * 9 = 72 MHz */
+        RCC.*.CFGR &= (~@as(u32, RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
+        RCC.*.CFGR |= @as(u32, RCC_CFGR_PLLSRC_HSE | RCC_CFGR_PLLMULL9);
+
+        //* Enable PLL */
+        RCC.*.CR |= RCC_CR_PLLON;
+
+        //* Wait till PLL is ready */
+        while ((RCC.*.CR & RCC_CR_PLLRDY) == 0) {}
+
+        //* Select PLL as system clock source */
+        RCC.*.CFGR &= (~@as(u32, RCC_CFGR_SW));
+        RCC.*.CFGR |= @as(u32, RCC_CFGR_SW_PLL);
+
+        //* Wait till PLL is used as system clock source */
+        while ((RCC.*.CFGR & @as(u32, RCC_CFGR_SWS)) != @as(u32, 0x08)) {}
+    } else { //* If HSE fails to start-up, the application will have wrong clock
+        //  configuration. User can add here some code to deal with this error */
+    }
 }
 
 fn nmiHandler() callconv(.C) noreturn {
@@ -153,10 +233,25 @@ const VECT_TAB_OFFSET = 0x0;
 const PERIPH_BASE = 0x40000000;
 const APB2PERIPH_BASE = PERIPH_BASE + 0x10000;
 const AHBPERIPH_BASE = PERIPH_BASE + 0x20000;
-const GPIOC_BASE = APB2PERIPH_BASE + 0x1000;
-const RCC_BASE = AHBPERIPH_BASE + 0x1000;
 pub const GPIO_PIN_13: u32 = 0x2000;
-pub const RCC_APB2Periph_GPIOC: u32 = 0x00000010;
+pub const RCC_APB2Periph_AFIO: u32   = 0x00000001;
+pub const RCC_APB2Periph_GPIOA: u32  = 0x00000004;
+pub const RCC_APB2Periph_GPIOB: u32  = 0x00000008;
+pub const RCC_APB2Periph_GPIOC: u32  = 0x00000010;
+pub const RCC_APB2Periph_GPIOD: u32  = 0x00000020;
+pub const RCC_APB2Periph_GPIOE: u32  = 0x00000040;
+pub const RCC_APB2Periph_GPIOF: u32  = 0x00000080;
+pub const RCC_APB2Periph_GPIOG: u32  = 0x00000100;
+pub const RCC_APB2Periph_ADC1: u32   = 0x00000200;
+pub const RCC_APB2Periph_ADC2: u32   = 0x00000400;
+pub const RCC_APB2Periph_TIM1: u32   = 0x00000800;
+pub const RCC_APB2Periph_SPI1: u32   = 0x00001000;
+pub const RCC_APB2Periph_TIM8: u32   = 0x00002000;
+pub const RCC_APB2Periph_USART1: u32 = 0x00004000;
+pub const RCC_APB2Periph_ADC3: u32   = 0x00008000;
+pub const RCC_APB2Periph_TIM9: u32   = 0x00080000;
+pub const RCC_APB2Periph_TIM10: u32  = 0x00100000;
+pub const RCC_APB2Periph_TIM11: u32  = 0x00200000;
 const RCC_CR_HSEON: u32 = 0x00010000;
 const RCC_CR_HSERDY: u32 = 0x00020000;
 const HSE_STARTUP_TIMEOUT: u16 = 0x0500;
@@ -177,7 +272,6 @@ const RCC_CR_PLLRDY: u32 = 0x02000000;
 const RCC_CFGR_SW: u32 = 0x00000003;
 const RCC_CFGR_SW_PLL: u32 = 0x00000002;
 const RCC_CFGR_SWS: u32 = 0x0000000C;
-const FLASH_R_BASE: u32 = AHBPERIPH_BASE + 0x2000;
 
 const GPIO_t = packed struct {
     CRL: u32,
@@ -188,6 +282,8 @@ const GPIO_t = packed struct {
     BRR: u32,
     LCKR: u32,
 };
+const GPIOC_BASE = APB2PERIPH_BASE + 0x1000;
+pub const GPIOC = @intToPtr(*volatile GPIO_t, GPIOC_BASE);
 
 const RCC_t = packed struct {
     CR: u32,
@@ -201,6 +297,8 @@ const RCC_t = packed struct {
     BDCR: u32,
     CSR: u32,
 };
+const RCC_BASE = AHBPERIPH_BASE + 0x1000;
+pub const RCC = @intToPtr(*volatile RCC_t, RCC_BASE);
 
 const FLASH_t = packed struct {
     ACR: u32,
@@ -214,94 +312,17 @@ const FLASH_t = packed struct {
     WRPR: u32,
 };
 
-pub const GPIOC = @intToPtr(*volatile GPIO_t, GPIOC_BASE);
-pub const RCC = @intToPtr(*volatile RCC_t, RCC_BASE);
+const FLASH_R_BASE: u32 = AHBPERIPH_BASE + 0x2000;
 pub const FLASH = @intToPtr(*volatile FLASH_t, FLASH_R_BASE);
 
-// copied verbatim from STM32 SDK
-fn systemInit() void {
-    //* Reset the RCC clock configuration to the default reset state(for debug purpose) */
-    //* Set HSION bit */
-    RCC.*.CR |= @as(u32, 0x00000001);
+const USART_t = packed struct {
+    SR: u32, DR: u32, BRR: u32, CR1: u32, CR2: u32, CR3: u32, GTPR: u32
+};
+const USART1_BASE: u32 = APB2PERIPH_BASE + 0x3800;
+pub const USART1 = @inttoptr(*volatile UART_t, UART1_BASE);
 
-    //* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
-    RCC.*.CFGR &= @as(u32, 0xF8FF0000);
+const USART2_BASE: u32 = PERIPH_BASE + 0x4400;
+pub const USART2 = @inttoptr(*volatile UART_t, UART1_BASE);
 
-    //* Reset HSEON, CSSON and PLLON bits */
-    RCC.*.CR &= @as(u32, 0xFEF6FFFF);
-
-    //* Reset HSEBYP bit */
-    RCC.*.CR &= @as(u32, 0xFFFBFFFF);
-
-    //* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
-    RCC.*.CFGR &= @as(u32, 0xFF80FFFF);
-
-    //* Disable all interrupts and clear pending bits  */
-    RCC.*.CIR = 0x009F0000;
-
-    //* Configure the System clock frequency, HCLK, PCLK2 and PCLK1 prescalers */
-    //* Configure the Flash Latency cycles and enable prefetch buffer */
-    setSysClock();
-
-    SCB.*.VTOR = FLASH_BASE | VECT_TAB_OFFSET; //* Vector Table Relocation in Internal FLASH. */
-}
-
-fn setSysClock() void {
-    var StartUpCounter: u32 = 0;
-    var HSEStatus: u32 = 0;
-
-    //* SYSCLK, HCLK, PCLK2 and PCLK1 configuration ---------------------------*/
-    //* Enable HSE */
-    RCC.*.CR |= @as(u32, RCC_CR_HSEON);
-
-    //* Wait till HSE is ready and if Time out is reached exit */
-    HSEStatus = RCC.*.CR & RCC_CR_HSERDY;
-    StartUpCounter += 1;
-    while ((HSEStatus == 0) and (StartUpCounter != HSE_STARTUP_TIMEOUT)) {
-        HSEStatus = RCC.*.CR & RCC_CR_HSERDY;
-        StartUpCounter += 1;
-    }
-
-    if ((RCC.*.CR & RCC_CR_HSERDY) != RESET) {
-        HSEStatus = 0x01;
-    } else {
-        HSEStatus = 0x00;
-    }
-
-    if (HSEStatus == 0x01) {
-        //* Enable Prefetch Buffer */
-        FLASH.*.ACR |= FLASH_ACR_PRFTBE;
-
-        //* Flash 2 wait state */
-        FLASH.*.ACR &= @as(u32, ~FLASH_ACR_LATENCY);
-        FLASH.*.ACR |= @as(u32, FLASH_ACR_LATENCY_2);
-
-        //* HCLK = SYSCLK */
-        RCC.*.CFGR |= @as(u32, RCC_CFGR_HPRE_DIV1);
-
-        //* PCLK2 = HCLK */
-        RCC.*.CFGR |= @as(u32, RCC_CFGR_PPRE2_DIV1);
-
-        //* PCLK1 = HCLK */
-        RCC.*.CFGR |= @as(u32, RCC_CFGR_PPRE1_DIV2);
-
-        //*  PLL configuration: PLLCLK = HSE * 9 = 72 MHz */
-        RCC.*.CFGR &= (~@as(u32, RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
-        RCC.*.CFGR |= @as(u32, RCC_CFGR_PLLSRC_HSE | RCC_CFGR_PLLMULL9);
-
-        //* Enable PLL */
-        RCC.*.CR |= RCC_CR_PLLON;
-
-        //* Wait till PLL is ready */
-        while ((RCC.*.CR & RCC_CR_PLLRDY) == 0) {}
-
-        //* Select PLL as system clock source */
-        RCC.*.CFGR &= (~@as(u32, RCC_CFGR_SW));
-        RCC.*.CFGR |= @as(u32, RCC_CFGR_SW_PLL);
-
-        //* Wait till PLL is used as system clock source */
-        while ((RCC.*.CFGR & @as(u32, RCC_CFGR_SWS)) != @as(u32, 0x08)) {}
-    } else { //* If HSE fails to start-up, the application will have wrong clock
-        //  configuration. User can add here some code to deal with this error */
-    }
-}
+const USART3_BASE: u32 = PERIPH_BASE + 0x4800;
+pub const USART3 = @inttoptr(*volatile UART_t, UART1_BASE);
